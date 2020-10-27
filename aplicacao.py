f6cdc1ff003c2128486d95cceb1cf776e5e583ea
Juan Lima <69512975+JuanLima10@users.noreply.github.com>
2020-10-27 09:02:31 -0300

    Add files via upload

---
+++ aplicacao.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+@app.route("/competidores")
+def competidor():
+    return render_template("competidores.html")
+
+app.run()
